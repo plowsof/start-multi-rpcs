b7b43b0924c44d9b75ef8ebf5edae3a80b88f4d1
plowsof <77655812+plowsof@users.noreply.github.com>
2022-05-10 03:16:43 +0100

    Update testing.py

--- testing.py
+++ testing.py
@@ -7,6 +7,12 @@ import threading
 import math
 import shutil
 from bs4 import BeautifulSoup
+import random
+import sqlite3
+
+# global block list https://gui.xmr.pm/files/block.txt
+get_info_template = ['adjusted_time', 'alt_blocks_count', 'block_size_limit', 'block_size_median', 'block_weight_limit', 'block_weight_median', 'bootstrap_daemon_address', 'busy_syncing', 'credits', 'cumulative_difficulty', 'cumulative_difficulty_top64', 'database_size', 'difficulty', 'difficulty_top64', 'free_space', 'grey_peerlist_size', 'height', 'height_without_bootstrap', 'incoming_connections_count', 'mainnet', 'nettype', 'offline', 'outgoing_connections_count', 'rpc_connections_count', 'stagenet', 'start_time', 'status', 'synchronized', 'target', 'target_height', 'testnet', 'top_block_hash', 'top_hash', 'tx_count', 'tx_pool_size', 'untrusted', 'update_available', 'version', 'was_bootstrap_ever_used', 'white_peerlist_size', 'wide_cumulative_difficulty', 'wide_difficulty']
+
 
 def init_monero_rpc(rpc_port,num_wallets,height):
     rpc_url = f"http://localhost:{rpc_port}/json_rpc"
@@ -59,11 +65,49 @@ def rpc_wallet_online(rpc_con):
             time.sleep(1)
             num_retries += 1
 
+def get_tor_session():
+    session = requests.session()
+    # Tor uses the 9050 port as the default socks port
+    session.proxies = {'http':  'socks5h://127.0.0.1:9050',
+                       'https': 'socks5h://127.0.0.1:9050'}
+    return session
+
 # torsocks --port 9150 /Applications/monero-wallet-gui.app/Contents/MacOS/monero-wallet-gui
 def open_wallet_transfer(rpc_port,remote_node,wallet):
+    global get_info_template
     #remote_node = "xmr-lux.boldsuck.org:38081"
 
     rpc_url = f"http://localhost:{rpc_port}/json_rpc"
+    # check if we're online + synched
+    get_info = f'{remote_node.split("/json_rpc")[0]}/get_info'
+    try:
+        if ".onion" in get_info:
+            ses = get_tor_session()
+            r = ses.get(get_info,timeout=60)
+        else:
+            r = requests.get(get_info,timeout=60)
+        lol = list(r.json().keys())
+        if lol == get_info_template:
+            print(f"{get_info} nice")
+        else:
+            print(f"{get_info} naughty")
+            con = sqlite3.connect('santas_xmr_list.db')
+            cur = con.cursor()
+            sql = '''INSERT OR IGNORE INTO naughty(ip) VALUES(?)'''
+            cur.execute(sql, (remote_node,))
+            con.commit()
+            con.close()
+            if r.json()["status"] == "Client signature does not verify for get_info":
+                return
+            if r.json().get("restricted") == True:
+                print("restwicted")
+        if r.json()["synchronized"] != True:
+            # not synced
+            return
+    except Exception as e:
+        print(e)
+        # its offline / slow
+        return
 
     rpc_args = [ 
         f"./monero-wallet-rpc", 
@@ -114,21 +158,34 @@ def open_wallet_transfer(rpc_port,remote_node,wallet):
         }
     try:
         r = requests.post(rpc_url, json={"jsonrpc":"2.0","id":"0","method":"transfer","params":params},timeout=120)
-        print(f"Node: {remote_node}")
-        print(r.json()["result"]["fee"])
+        
+        fee = r.json()["result"]["fee"]
+        print(fee)
+        con = sqlite3.connect('santas_xmr_list.db')
+        cur = con.cursor()
+        sql = '''INSERT OR IGNORE INTO fees(ip,fee) VALUES(?,?)'''
+        cur.execute(sql, (remote_node,fee))
+        con.commit()
+        con.close()
+        if fee > 8230000:
+            print(f"Node: {remote_node}")
+            print(r.json()["result"]["fee"])
         rpc_connection.close_wallet()
         monero_daemon.terminate()
+        monero_daemon.wait()
     except Exception as e:
         print(e)
         monero_daemon.terminate()
+        monero_daemon.wait()
 
 def threaded_test(port,nodes,wallet):
     for node in nodes:
-        #node = f'{node["hostname"]}:{node["port"]}'
-        print(f"{port} {node} {wallet}")
+        #node = f'{node["hostname"]}:{node["port"]}'node1.xmr-tw.org:18081
+        #print(f"{port} {node} {wallet}")
         open_wallet_transfer(port,node,str(wallet))
 
 def main():
+    '''
     response = requests.get("https://monero.fail/?nettype=mainnet")
     webpage = response.content
     stagenet = []
@@ -138,10 +195,36 @@ def main():
         for value in values:
             if "http" in value.text:
                 stagenet.append(value.text)
+    '''
+    # get peer list ips
+    con = sqlite3.connect('santas_xmr_list.db')
+    cur = con.cursor()
+    create_naughty_table = """ CREATE TABLE IF NOT EXISTS naughty (
+                                ip text PRIMARY KEY
+                            ); """
+    create_fee_table = """ CREATE TABLE IF NOT EXISTS fees (
+                            ip text,
+                            fee integer
+                        ); """
+    cur.execute(create_fee_table)
+    cur.execute(create_naughty_table)
+    con.commit()
+    con.close()
+    
+    r = requests.get("http://192.168.1.68:18081/get_peer_list").json()
 
-    num_wallets = 10
+    node_list = []
+    for x in r["white_list"]:
+        if x.get("rpc_port"):
+            if "::ffff:" in x["host"]:
+                x['host'] = x['host'].split("::ffff:")[1]   
+            host = f"http://{x['host']}:{x['rpc_port']}/json_rpc"
+            node_list.append(host)
+
+    stagenet = node_list
+    num_wallets = 3
+    random.shuffle(stagenet)
     per_thread = len(stagenet) / num_wallets
-
     counter = 1
     total = len(stagenet)
 
@@ -165,15 +248,14 @@ def main():
     wallet_counter = 0
     for i in range(len(stagenet)):
         the_list[wallet_counter].append(stagenet[i])
-        if wallet_counter == 9:
+        if wallet_counter == (num_wallets - 1):
             wallet_counter = 0
         else:
             wallet_counter += 1
-
+    pprint.pprint(the_list)
     for l in the_list:
         t = threading.Thread(target=threaded_test, args=(wallet_port[l],the_list[l],l,))
         t.start()
-
 if __name__ == "__main__":
     #height = requests.get("http://busyboredom.com:18081/get_info").json()["height"]
     '''
